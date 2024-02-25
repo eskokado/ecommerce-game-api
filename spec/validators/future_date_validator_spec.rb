7d336d53bf162cf6cf9bf010449e4c55565a01be
eskokado <eskokado@gmail.com>
2024-02-25 07:46:36 -0300

    test: future date validator custom when date is greater than current date

--- spec/validators/future_date_validator_spec.rb
+++ spec/validators/future_date_validator_spec.rb
@@ -34,4 +34,12 @@ describe FutureDateValidator do
       expect(subject.errors.keys).to include(:date)
     end
   end
+
+  context "when date is greater than current date" do
+    before { subject.date = Time.zone.now + 1.day }
+
+    it "should be valid" do
+      expect(subject.valid?).to be_truthy
+    end
+  end
 end
